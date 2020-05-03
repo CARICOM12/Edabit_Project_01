fa9911c21f6e89ce1d99d9604988db4b4229e215
CARICOM12 <0190a@ukr.net>
2020-05-03 15:11:51 +0300

    Add files via upload

---
+++ Edabit_Progect_01/Edabit_Progect_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Edabit_Progect_01
+{
+    class Program
+    {
+        public static bool TimeForMilkAndCookies(int year, int month, int day)
+        {
+            return (day == 24 && month == 11);
+        }
+    }
+}
+/*Christmas Eve is almost upon us, so naturally we need to prepare some 
+ * milk and cookies for Santa! Create a function that accepts year, month 
+ * and day as integers and returns true if it's Christmas Eve (December 24th) 
+ * and false otherwise.
